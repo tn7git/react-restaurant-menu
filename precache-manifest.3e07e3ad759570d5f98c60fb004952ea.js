cd7987b4ed4d0f3e4bc0c449717966526ba88a0c
tn7git <90912178+tn7git@users.noreply.github.com>
2023-08-13 18:14:41 +1000

    Updates

---
+++ precache-manifest.3e07e3ad759570d5f98c60fb004952ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75f2289459feb983f397a569806179e",
+    "url": "/react-restaurant-menu/index.html"
+  },
+  {
+    "revision": "f671ee96ea7fd174b6c7",
+    "url": "/react-restaurant-menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-restaurant-menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-restaurant-menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f671ee96ea7fd174b6c7",
+    "url": "/react-restaurant-menu/static/js/main.40f028f0.chunk.js"
+  },
+  {
+    "revision": "9edf4ab740199dc58e68",
+    "url": "/react-restaurant-menu/static/js/runtime-main.7962da00.js"
+  }
+]);
